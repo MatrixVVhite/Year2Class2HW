659127c47ea7ad96915bfcaa3e5e53f726c63e8f
NeoThePotato <neorayb@gmail.com>
2024-02-19 18:56:55 +0200

    Implemented character movement.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,14 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
+    public UnityEvent<float> onCharacterVelocityChange;
     [SerializeField] Rigidbody _rb;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _movementSpeedMultiplier = 100f;
+	private Vector2 _move;
+	private float _previousCharacterVelocity = 0f;
 
-    private void OnMove()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        _move = context.ReadValue<Vector2>();
     }
+
+	private void FixedUpdate()
+	{
+        _rb.AddForce(new Vector3(_move.x, 0, _move.y) * _movementSpeedMultiplier);
+        float currentCharacterVelocity = _rb.velocity.magnitude;
+		if (currentCharacterVelocity != _previousCharacterVelocity)
+            onCharacterVelocityChange.Invoke(currentCharacterVelocity);
+        _previousCharacterVelocity = currentCharacterVelocity;
+	}
 }
