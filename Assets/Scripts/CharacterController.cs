5575780e6495747f2b63cb0fae8ac6baf084365e
MatrixVVhite <yotamoscohen@gmail.com>
2024-02-19 20:43:18 +0200

    Fixed camera position to over the shoulder

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,7 +6,9 @@ public class CharacterController : MonoBehaviour
 {
     public event UnityAction<float> onCharacterVelocityChange;
     [SerializeField] Rigidbody _rb;
-    [SerializeField] private float _movementSpeedMultiplier = 100f;
+	[SerializeField] float _walkVelocity = 100f;
+	[SerializeField] float _sprintVelocity = 200f;
+	private float _movementVelocityMultiplier;
 	private UnityEvent<float> onCharacterVelocityChangeEvent = new();
 	private Vector2 _move;
 	private Vector3 _previousCharacterPosition;
@@ -16,6 +18,7 @@ public class CharacterController : MonoBehaviour
 
 	private void Awake()
 	{
+		_movementVelocityMultiplier = _walkVelocity;
 		if (onCharacterVelocityChangeEvent != null)
 			onCharacterVelocityChangeEvent.AddListener(onCharacterVelocityChange);
 	}
@@ -25,9 +28,17 @@ public class CharacterController : MonoBehaviour
         _move = context.ReadValue<Vector2>();
     }
 
+	public void OnSprint(InputAction.CallbackContext context)
+	{
+		if (context.started)
+			_movementVelocityMultiplier = _sprintVelocity;
+		if (context.canceled)
+			_movementVelocityMultiplier = _walkVelocity;
+	}
+
 	private void FixedUpdate()
 	{
-        _rb.AddForce(new Vector3(_move.x, 0, _move.y) * _movementSpeedMultiplier, ForceMode.Impulse);
+        _rb.AddForce(new Vector3(_move.x, 0, _move.y) * _movementVelocityMultiplier, ForceMode.Impulse);
 		if (CurrentCharacterVelocity != _previousCharacterVelocity)
             onCharacterVelocityChangeEvent.Invoke(CurrentCharacterVelocity);
         _previousCharacterPosition = _rb.position;
