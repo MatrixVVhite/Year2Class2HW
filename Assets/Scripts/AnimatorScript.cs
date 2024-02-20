480f8282a8d2d324d9d5a50bad8364d65dedd369
NeoThePotato <neorayb@gmail.com>
2024-02-20 16:27:57 +0200

    Implemented model rotation by walk direction.

--- Assets/Scripts/AnimatorScript.cs
+++ Assets/Scripts/AnimatorScript.cs
@@ -9,15 +9,23 @@ public class AnimatorScript : MonoBehaviour
 	private void OnEnable()
 	{
         _characterController.onCharacterVelocityChange += UpdateCharacterSpeed;
+		_characterController.onCharacterDirectionChange += UpdateCharacterRotation;
 	}
 
 	private void OnDisable()
 	{
 		_characterController.onCharacterVelocityChange -= UpdateCharacterSpeed;
+		_characterController.onCharacterDirectionChange -= UpdateCharacterRotation;
 	}
 
 	void UpdateCharacterSpeed(float speed)
     {
         _animator.SetFloat(_characterSpeed, speed);
     }
+
+	void UpdateCharacterRotation(Vector2 rotation)
+	{
+		if (rotation != Vector2.zero)
+			transform.rotation = Quaternion.Euler(0, Vector2.SignedAngle(rotation, Vector2.up), 0);
+	}
 }
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,22 +5,28 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     public event UnityAction<float> onCharacterVelocityChange;
-    [SerializeField] Rigidbody _rb;
+	public event UnityAction<Vector2> onCharacterDirectionChange;
+	[SerializeField] Rigidbody _rb;
 	[SerializeField] float _walkVelocity = 100f;
 	[SerializeField] float _sprintVelocity = 200f;
 	private float _movementVelocityMultiplier;
 	private UnityEvent<float> onCharacterVelocityChangeEvent = new();
+	private UnityEvent<Vector2> onCharacterDirectionChangeEvent = new();
 	private Vector2 _move;
 	private Vector3 _previousCharacterPosition;
 	private float _previousCharacterVelocity;
+	private Vector2 _previousCharacterDirection;
 
 	private float CurrentCharacterVelocity => (_previousCharacterPosition - _rb.position).magnitude/Time.fixedDeltaTime;
+	private Vector2 CurrentCharacterDirection => _move.normalized;
 
 	private void Awake()
 	{
 		_movementVelocityMultiplier = _walkVelocity;
 		if (onCharacterVelocityChangeEvent != null)
 			onCharacterVelocityChangeEvent.AddListener(onCharacterVelocityChange);
+		if (onCharacterDirectionChangeEvent != null)
+			onCharacterDirectionChangeEvent.AddListener(onCharacterDirectionChange);
 	}
 
 	public void OnMove(InputAction.CallbackContext context)
@@ -41,7 +47,10 @@ public class CharacterController : MonoBehaviour
         _rb.AddForce(new Vector3(_move.x, 0, _move.y) * _movementVelocityMultiplier, ForceMode.Impulse);
 		if (CurrentCharacterVelocity != _previousCharacterVelocity)
             onCharacterVelocityChangeEvent.Invoke(CurrentCharacterVelocity);
+		if (CurrentCharacterDirection != _previousCharacterDirection)
+			onCharacterDirectionChangeEvent.Invoke(CurrentCharacterDirection);
         _previousCharacterPosition = _rb.position;
 		_previousCharacterVelocity = CurrentCharacterVelocity;
+		_previousCharacterDirection = CurrentCharacterDirection;
 	}
 }
